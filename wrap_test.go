9a00577243c759d5cebadaa0c9e6fb09926cb977
Joseph <wangjianhua@ztgame.com>
2021-12-27 17:17:10 +0800

    1. add test

--- wrap_test.go
+++ wrap_test.go
@@ -12,6 +12,12 @@ func TestLogger_Info(t *testing.T) {
 	l.WithFields(Fields{"b": 88, "a": []interface{}{99.99, "www"}}).Info(1, 2, 3, 4, false, []interface{}{11.11, "x"})
 }
 
+func TestLogger_Warn(t *testing.T) {
+	OnlyConsole()
+	l := &Logger{}
+	l.WithField("x", []interface{}{22.31, "yry"}).Warn(1, 2, 3, 4, false, []interface{}{11.11, "x"})
+}
+
 func TestFields_ZapFields(t *testing.T) {
 	l := &Logger{}
 	println(fmt.Sprint(l.FieldMap.ZapFields()))
